da085d6e1f279e85f2f5492988fa8415706607a4
PogoNguyen03 <105332052+PogoNguyen03@users.noreply.github.com>
2025-12-19 17:43:50 +0700

    update
    update lag code

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200], 
-    minimumCacheTTL: 60,
   },
 };
 
